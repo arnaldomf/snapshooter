bbcf1c3dd09d3e2593f7cef53dd16e9a1fb00494
arnaldo.filho <arnaldo.filho@dafiti.com.br>
2015-12-11 18:01:54 -0200

    Validates windowHour

--- models/instances.go
+++ models/instances.go
@@ -1,5 +1,25 @@
 package models
 
+import (
+	"errors"
+	"strconv"
+)
+
 // Instance is an interface to cloud Instances
 type Instance interface {
+	windowHour(hh int)
+}
+
+// SetWindowHour validates the window_hour attribute and parses it to int
+func SetWindowHour(instance Instance, hour string) error {
+	hh, err := strconv.Atoi(hour)
+	if err != nil {
+		return err
+	}
+	if hh < 0 || hh > 23 {
+		return errors.New("WindowHour is not valid")
+	}
+	instance.windowHour(hh)
+
+	return nil
 }
